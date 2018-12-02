3d9d68f807bb5d2203b66f8cf4b467d6d015df05
Antti Ketola <antti@anttiketola.com>
2018-12-02 18:39:20 +0200

    Week 5 stuff

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,9 @@
-# Antti Ketola, 25.11.2018. This is the data wrangling script for week 4 workshop.
+# Antti Ketola, 29.11.2018. This is the data wrangling script for week 5 workshop.
+# This script continues week 4 script - The week 5 part is at the bottom, starting from a comment indicating week change
+# The script will continue to work on the human.CSV file that is created during week 4 wrangling
+
+
+# --- WEEK 4 Data wrangling ----
 
 # Reading the two datasets from the given links
 
@@ -46,12 +51,58 @@ str(human)
 
 write.csv(human, file = "data/human.CSV", row.names = FALSE)
 
-# Reading it once more and checking it
+# --- WEEK 5 Data wrangling ----
 
-human_check = read.csv("data/human.csv")
-str(human_check)
+# Loading the data again from the file this script worked on before
 
+human <- read.csv("data/human.csv")
 
+# Note, explanations of the variables and the data will be in the diary 
+# (it appears to me the assignment part was in the wrong place)
+# Exploring the structure and dimensions of the data, Checking that the data has correctly 195 obs. and 19 var.
 
+str(human)
+dim(human)
+summary(human)
 
+# Mutating, transforming the GNI variable to numeric
+
+library(stringr)
+
+human$GNI <- str_replace(human$GNI, pattern=",", replace ="") %>% as.numeric
+
+# Keeping only the wanted variables, In this dataset, as renamed in week 4, the variables to keep are:
+# Country, Edu2Ratio, LabourRatio, EduExp, LifeExp, GNI, MatMorRatio, AdoBirth, ParliamentF
+
+keep <- c("Country", "Edu2Ratio", "LabourRatio", "LifeExp", "EduExp", "GNI", "MatMorRatio", "AdoBirth", "ParliamentF")
+human <- select(human, one_of(keep))
+
+# Removing all rows with missing values
+
+human <- filter(human, complete.cases(human))
+
+# Removing observations which relate to regions instead of countries
+
+# Taking look at the last 10 obsv, choosing last indice we want to keep and choosing all until that
+
+tail(human, 10)
+last <- nrow(human) - 7
+human <- human[1:last, ]
+
+# Defining row names of data by the country name
+
+rownames(human) <- human$Country
+
+# And removing the country column..
+
+human <- select(human, -Country)
+
+# The dataset should now have 155 obsv. and 8 variables, confirming this next
+
+str(human)
+
+# And as confirmed, it has 155 obsv. and 8 variables.
+# Saving the data including row names, overwriting the old CSV
+
+write.csv(human, file = "data/human.csv", row.names = TRUE)
 
